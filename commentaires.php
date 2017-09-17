c73c14e3c0db324ebdfbff55f134ad2e9e133169
sanouo <sanouo591@gmail.com>
2017-09-17 16:25:40 +0200

    tp blog

---
+++ commentaires.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html class="no-js" lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Blog-PHP</title>
+        <meta name="description" content="blog sur le langage php">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="icon" href="favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=Frijole" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Frijole|Roboto" rel="stylesheet">
+
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+        <header>
+          <h1>BLOG</h1>
+        </header>
+
+
+<?php
+
+   try
+   {
+   $bdd = new PDO('mysql:host=localhost;dbname=blogexo;charset=utf8', 'phpmyadmin', 'sana15');
+   }
+   catch (Exception $e)
+   {
+       die('Erreur : ' . $e->getMessage());
+   }
+
+
+?>
+
+
+<?php
+    $reponse = $bdd->prepare('SELECT * FROM billets WHERE id = ?');
+    $reponse->execute(array($_GET['recupinfo']));
+    $donnees = $reponse->fetch();
+
+?>
+
+<div class="article">
+        <p class="titr"><?php echo htmlspecialchars($donnees['titre']); ?></p> <br>
+        <p class="conten"><?php echo htmlspecialchars($donnees ['contenu']); ?></p> <br>
+        <p class="dat"><?php echo htmlspecialchars($donnees['date_creation']); ?></p>
+        </div>
+
+        <?php
+        $reponse->closeCursor();
+        ?>
+
+
+
+<h3>Commentaires :</h3>
+
+<?php
+    $reponse = $bdd->query('SELECT * FROM commentaires LIMIT 0, 5');
+    while ($donnees = $reponse->fetch())
+    {
+    ?>
+
+
+    <div class="article2">
+     <p class="commen"><?php echo htmlspecialchars($donnees['auteur']); ?>
+      <?php echo htmlspecialchars($donnees['date_commentaire']); ?><br>
+     <?php echo htmlspecialchars($donnees['commentaire']); ?></p>
+    </div>
+
+    <?php
+    }
+    $reponse->closeCursor();
+    ?>
+    <a class="bouton" href="index.php">Retour</a>
+
+
+
+
+                <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+                <script src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js" integrity="{{JQUERY_SRI_HASH}}" crossorigin="anonymous"></script>
+                <script>window.jQuery || document.write('<script src="js/vendor/jquery-{{JQUERY_VERSION}}.min.js"><\/script>')</script>
+                <script src="js/plugins.js"></script>
+                <script src="js/main.js"></script>
+
+                <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+                <script>
+                    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+                    ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+                </script>
+                <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+            </body>
+        </html>
